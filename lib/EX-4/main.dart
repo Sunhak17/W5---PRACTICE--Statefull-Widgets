811dce5f4422c477ba4fe246904a4a9c2fdf0fde
Oeng Sunhak <sunhak963@gmail.com>
2025-11-19 21:36:28 +0700

    first commit

--- lib/EX-4/main.dart
+++ lib/EX-4/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Score()));
+    runApp(MaterialApp(
+        debugShowCheckedModeBanner: false, 
+        home: Score()
+    )
+);
 
 class Score extends StatelessWidget {
   const Score({super.key});
